f9e3c8213ffbd6fd10aad8b6e0e452b5d5c5e83b
Molly Wood <mollywood@Interzone.localdomain>
2018-05-01 12:49:47 -0500

    inital commit

---
+++ add2.py
@@ -0,0 +1,7 @@
+# takes the user input
+num1 = int(input("Input a number: "))
+num2 = int(input("Input a second number: "))
+
+# adds the numbers and prints
+
+print(f"The sum of these numbers is {num1 + num2}.")
